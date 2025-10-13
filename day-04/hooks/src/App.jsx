1c47d2a856d09e319ef1ec4b8aa46c61a3f559ff
yashvi-param <yashvirwn123@gmail.com>
2025-10-13 21:18:23 +0530

    Add files via upload
    complete

--- day-04/hooks/src/App.jsx
+++ day-04/hooks/src/App.jsx
@@ -1,14 +1,72 @@
-import UseState from "./Hooks-lifeycle/Usestate";
+// import UseState from "./hooks/UseState";
 
+// import { useState } from "react";
+import Conditional from "./events/Conditional";
+import OnChange from "./events/OnChange";
+import OnClick from "./events/OnClick";
+import OnSubmit from "./events/OnSubmit";
+import PassingArgument from "./events/PassingArgument";
+import List from "./lists/List";
+import ObjectList from "./lists/ObjectList";
+import Focus from "./refs/Focus";
+import Uncontrolled from "./refs/Uncontrolled";
 
+// import UseEffect from "./hooks/UseEffect";
+
+// import { useState } from "react";
+// import LifeCycle from "./hooks/LifeCycle";
+// import ObjectUseState from "./hooks/ObjectUseState";
 
 const App = () => {
+  // const [show, setShow] = useState(true);
+
+  // const [input, setInput] = useState("");
   return (
     <>
-      <UseState />
+      {/* <UseState /> */}
+      {/* <ObjectUseState /> */}
+
+      {/* <button onClick={() => setShow((prev) => !prev)}>
+        {show ? "hide" : "show"}{" "}
+      </button> */}
+
+      {/* {show && <LifeCycle />} */}
+
+      {/* <UseEffect /> */}
+
+      {/* events */}
+
+      {/* <OnClick /> */}
+
+      {/* <OnChange /> */}
+
+      {/* <OnSubmit /> */}
+
+      {/* passing argument in event using props  */}
+
+      {/* <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <PassingArgument name={input} /> */}
+      {/* <PassingArgument /> */}
+
+      {/* conditional rendering example */}
+      {/* <Conditional /> */}
+
+      {/* lists */}
+
+      {/* <List /> */}
+
+      {/* <ObjectList /> */}
+
+      {/* ref */}
+
+      <Focus />
     </>
   );
 };
 
-
-export default App
+export default App;
